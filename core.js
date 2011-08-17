a8f6a373c0f91463a8c0d648b52ba4b5ff28fa8f
Lucas Riutzel <lriutzel@gmail.com>
2011-08-16 21:26:46 -0500

    initial drupal submodule commit

---
+++ core.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   Drupal
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Drupal */
+
+/** @namespace
+
+  My cool new framework.  Describe your framework.
+  
+  @extends SC.Object
+*/
+Drupal = SC.Object.create(
+  /** @scope Drupal.prototype */ {
+
+  NAMESPACE: 'Drupal',
+  VERSION: '0.1.0',
+
+  // TODO: Add global constants or singleton objects needed by your app here.
+
+}) ;
